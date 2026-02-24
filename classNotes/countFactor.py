037c2768168ed3fd09b4fc8fdc0c8e55f62ac8d9
marlin-reso <sanju.reso@gmail.com>
2026-02-24 21:40:00 +0530

    class notes

---
+++ classNotes/countFactor.py
@@ -0,0 +1,30 @@
+def printFactorOfNum():
+    user_input = int(input("Enter the number : "))
+    count = 0
+    for i in range (1, user_input):
+        if user_input%i ==0:
+            count+=1
+            print(i, end=" ")
+    print(f"\nthere are {count} factors of {user_input}")
+#printFactorOfNum()
+
+def countDigit():
+    user_input = int(input("Enter the digit : "))
+    count = 0
+    while user_input>0:
+        user_input= user_input//10
+        count+=1
+    print(f"{count} is count of digit")
+#countDigit()
+
+def strongNumber():
+    user_input = int(input("Enter the number to check strong number : "))
+    sum =1
+    while user_input>0:
+        digit = user_input%10
+        for i in range (1,digit):
+            sum = sum + sum*i
+
+
+
+
--- geeksForGeeks/keywords.py
+++ geeksForGeeks/keywords.py
@@ -1,3 +1,31 @@
 import keyword
 print("The list of keywords are : ")
 print(keyword.kwlist)
+
+'''
+Category	            Keywords
+Value Keywords      :    True, False, None
+
+Operator Keywords	:    and, or, not, is, in
+
+Control Flow Keywords:  if, else, elif, for, while, break,
+                       continue, pass, try, except, finally, raise, assert
+
+Function and Class  :	def, return, lambda, yield, class
+
+Context Management	:      with, as
+
+Import and Module	:      import, from
+
+Scope and Namespace	:    global, nonlocal
+
+Async Programming	:     async, await
+
+non_used :
+'None','as', 'assert','async', 'await','continue',
+'del','finally','from', 'global','is', 'lambda',
+'nonlocal','pass', 'raise', 'with', 'yield']
+used : 
+False, True, and , break, class, def, elif, else, 
+except, for, if, import, in, not, or, return , try, while.
+'''
